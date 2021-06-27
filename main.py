359235c2e75f192f443f9c9e94127f46fca2ec88
Achyut-0705 <71813671+Achyut-0705@users.noreply.github.com>
2021-06-27 23:50:12 +0530

    'update'

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ import sqlite3 as sql
 
 myRest = None
 currAdmin = None
+bg_main = "#d8c3a5"
+btn_bg = "#eae7dc"
 
 class Login:
     def __init__(self, root):
         
         self.window = root
         
-        bg_main = "#d8c3a5"
-        btn_bg = "#eae7dc"
+        global bg_main
+        global btn_bg
         ft = tkFont.Font(family='Roboto',size=25)
         
         root.title("Restraunt Management System")
@@ -84,6 +86,7 @@ class Login:
         self.login_btn["text"] = "Admin Login"
         self.login_btn.place(x=200,y=280,width=150,height=30)
         self.login_btn["command"] = self.logAdmin
+        # self.login_btn.bind()
         
         self.login_btn=tk.Button(root)
         self.login_btn["bg"] = btn_bg
@@ -123,7 +126,8 @@ class Login:
 
 class AdminPanel:
     def __init__(self, root):
-        
+        global bg_main
+        global btn_bg
         self.window = root
         bg_main = "#d8c3a5"
         btn_bg = "#eae7dc"
@@ -137,6 +141,8 @@ class AdminPanel:
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
+        root.configure(background = bg_main)
+        
 
         self.admin=tk.Label(root)
         ft = tkFont.Font(family='Roboto',size=50,weight = "bold")
@@ -226,116 +232,137 @@ class AdminPanel:
         
 class SalesPanel:
     def __init__(self, root):
+        global bg_main
+        global btn_bg
         root.title("Sales Panel")
-        width=1200
-        height=740
+        width=900
+        height=570
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
+        
+        root.configure(background = bg_main)
 
         self.empLoginHead=tk.Label(root)
-        ft = tkFont.Font(family='Roboto',size=115)
+        ft = tkFont.Font(family='Roboto',size=50,weight = "bold")
         self.empLoginHead["font"] = ft
         self.empLoginHead["fg"] = "#333333"
         self.empLoginHead["justify"] = "center"
+        self.empLoginHead["bg"] = bg_main
         self.empLoginHead["text"] = "Place an order !"
-        self.empLoginHead.place(x=80,y=50,width=1068,height=125)
+        self.empLoginHead.place(x=200,y=30,width=500,height=50)
 
         self.label_name=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=28)
+        ft = tkFont.Font(family='Roboto',size=15, weight = "bold")
         self.label_name["font"] = ft
         self.label_name["fg"] = "#333333"
         self.label_name["justify"] = "center"
+        self.label_name["bg"] = bg_main
         self.label_name["text"] = "Name:"
-        self.label_name.place(x=260,y=190,width=115,height=51)
+        self.label_name.place(x=250,y=110,width=60,height=30)
 
         self.name_emp=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=28)
+        # ft = tkFont.Font(family='Times',size=28)
         self.name_emp["font"] = ft
         self.name_emp["fg"] = "#333333"
         self.name_emp["justify"] = "left"
+        self.name_emp["bg"] = bg_main
         self.name_emp["text"] = "Achyut Shukla"
-        self.name_emp.place(x=420,y=180,width=731,height=90)
+        self.name_emp.place(x=350,y=100,width=250,height=50)
 
         self.label_username=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=28)
+        # ft = tkFont.Font(family='Times',size=28)
         self.label_username["font"] = ft
         self.label_username["fg"] = "#333333"
         self.label_username["justify"] = "center"
+        self.label_username["bg"] = bg_main
         self.label_username["text"] = "Username:"
-        self.label_username.place(x=200,y=270,width=166,height=53)
+        self.label_username.place(x=210,y=155,width=100,height=30)
 
         self.username_emp=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=28)
+        # ft = tkFont.Font(family='Times',size=28)
         self.username_emp["font"] = ft
         self.username_emp["fg"] = "#333333"
         self.username_emp["justify"] = "left"
+        self.username_emp["bg"] = bg_main
         self.username_emp["text"] = "Achyut007"
-        self.username_emp.place(x=420,y=270,width=731,height=56)
+        self.username_emp.place(x=350,y=145,width=250,height=50)
 
         self.label_name_customer=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=28)
+        # ft = tkFont.Font(family='Times',size=28)
         self.label_name_customer["font"] = ft
         self.label_name_customer["fg"] = "#333333"
-        self.label_name_customer["justify"] = "center"
+        self.label_name_customer["justify"] = "left"
+        self.label_name_customer["bg"] = bg_main
         self.label_name_customer["text"] = "Enter Customer Name:"
-        self.label_name_customer.place(x=30,y=350,width=351,height=54)
+        self.label_name_customer.place(x=35,y=220,width=200,height=54)
 
         self.name_customer=tk.Entry(root)
         self.name_customer["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=28)
+        # ft = tkFont.Font(family='Times',size=28)
         self.name_customer["font"] = ft
         self.name_customer["fg"] = "#333333"
         self.name_customer["justify"] = "left"
-        self.name_customer["text"] = ""
-        self.name_customer.place(x=420,y=360,width=760,height=40)
+        self.name_customer.place(x=250,y=225,width=600,height=40)
 
         self.label_email=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=28)
+        # ft = tkFont.Font(family='Times',size=28)
         self.label_email["font"] = ft
         self.label_email["fg"] = "#333333"
         self.label_email["justify"] = "center"
+        self.label_email["bg"] = bg_main
         self.label_email["text"] = "Enter E-mail ID:"
-        self.label_email.place(x=140,y=430,width=229,height=48)
+        self.label_email.place(x=53,y=290,width=229,height=48)
 
         self.email_customer=tk.Entry(root)
         self.email_customer["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=28)
+        # ft = tkFont.Font(family='Times',size=28)
         self.email_customer["font"] = ft
         self.email_customer["fg"] = "#333333"
         self.email_customer["justify"] = "left"
         self.email_customer["text"] = ""
-        self.email_customer.place(x=420,y=430,width=760,height=40)
+        self.email_customer.place(x=250,y=290,width=600,height=40)
 
         self.list_item=tk.Listbox(root)
         self.list_item["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=28)
+        # ft = tkFont.Font(family='Times',size=28)
         self.list_item["font"] = ft
         self.list_item["fg"] = "#333333"
         self.list_item["justify"] = "left"
-        self.list_item.place(x=750,y=490,width=374,height=208)
+        self.list_item.place(x=500,y=380,width=374,height=160)
 
         self.btn_add_item=tk.Button(root)
         self.btn_add_item["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times',size=28)
+        # ft = tkFont.Font(family='Times',size=28)
         self.btn_add_item["font"] = ft
         self.btn_add_item["fg"] = "#000000"
         self.btn_add_item["justify"] = "center"
+        self.btn_add_item["bg"] = btn_bg
         self.btn_add_item["text"] = "Add Item"
-        self.btn_add_item.place(x=140,y=620,width=150,height=50)
+        self.btn_add_item.place(x=50,y=430,width=400,height=50)
         self.btn_add_item["command"] = self.btn_add_item_command
 
         self.btn_remove_item=tk.Button(root)
         self.btn_remove_item["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times',size=28)
+        # ft = tkFont.Font(family='Times',size=28)
         self.btn_remove_item["font"] = ft
         self.btn_remove_item["fg"] = "#000000"
         self.btn_remove_item["justify"] = "center"
+        self.btn_remove_item["bg"] = btn_bg
         self.btn_remove_item["text"] = "Remove"
-        self.btn_remove_item.place(x=390,y=620,width=150,height=50)
+        self.btn_remove_item.place(x=50,y=500,width=400,height=50)
         self.btn_remove_item["command"] = self.btn_remove_item_command
+        
+        items = ['maggie','momos','pizza']
+        clicked = tk.StringVar()
+        clicked.set('Select an item')
+        self.item_list = tk.OptionMenu(root,clicked,*items)
+        self.item_list['bg'] = btn_bg
+        self.item_list['font'] = ft
+        self.item_list['menu']['bg'] = btn_bg
+        self.item_list.place(x=50,y=350,width=400,height=50)
 
     def btn_add_item_command(self):
         pass
