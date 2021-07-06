7bea27200c1dad5bfec3bb3ddde6abef86d736a9
anu-10 <muralidharananupam.am@gmail.com>
2021-07-06 22:43:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -606,7 +606,18 @@ class SalesPanel:
         
             rec += f"\n-----------------\nTotal: {total}/- (Tax Inclusive)"
             self.receipt["text"] = rec
-
+            
+            frame = ttk.Frame(self.root, width=300, height=267)
+            frame.grid(row = 900,column = 350)
+            canvas = Canvas(frame)
+            canvas.pack(side = tk.LEFT)
+            scrollbar = ttk.Scrollbar(frame, orient="vertical", command=canvas.yview)
+            scrollbar.pack(side = tk.RIGHT, fill = 'y')
+            canvas.configure(yscrollcommand = scrollbar.set)
+            canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+            canvas2 = ttk.Frame(canvas)
+            canvas.create_window((0,0),window = canvas2, anchor = 'nw')
+            ttk.Label(canvas2, text = self.receipt['text']).pack()
             
             o.execute("INSERT INTO orders (cust_name, cust_email, total) VALUES (?, ?, ?)", (name, email, total))
             orderCon.commit()
